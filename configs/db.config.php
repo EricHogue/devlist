6041c1e2134aabc5e8bfa5bc53ea75e28d8226f2
bseekins <bseekinsDEV@gmail.com>
2015-01-15 15:30:59 -0500

    DB Config
    made the database connection file PDO

---
+++ configs/db.config.php
@@ -0,0 +1,13 @@
+<?php
+	// pdo database connection
+	try{
+		// connects using mysql connection information
+		$pdo = new PDO('mysql:host=localhost;dbname=devlist', 'root', '');
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->exec('SET NAMES "utf8"');
+	// if connection fails
+	}catch(PDOException $e){
+		echo "There was a problem connecting ot your datase" . "<br />" . $e->getMessage();
+		exit();
+	}
+?>
