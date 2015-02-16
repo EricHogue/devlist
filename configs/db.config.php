9a23687ea35da0a1ee259fc8b6510ba734d39193
bseekins <bseekinsDEV@gmail.com>
2015-02-15 19:26:29 -0500

    Updated Settings/DB
    updated settings and db configuration

--- configs/db.config.php
+++ configs/db.config.php
@@ -2,9 +2,8 @@
 	// pdo database connection
 	try{
 		// connects using mysql connection information
-		$pdo = new PDO('mysql:host=localhost;dbname=devlist', 'root', '');
+		$pdo = new PDO('mysql:host=localhost;dbname=devlist;charset=utf8', 'root', '');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$pdo->exec('SET NAMES "utf8"');
 	// if connection fails
 	}catch(PDOException $e){
 		echo "There was a problem connecting to your datase" . "<br />" . $e->getMessage();
--- configs/settings.config.php
+++ configs/settings.config.php
@@ -3,13 +3,12 @@
     try{
         $sql = 'SELECT * FROM settings ORDER BY id';
         $statement = $pdo->prepare($sql);
-        $statement->bindValue(':id', 'id');
         $statement->execute();
         $result = $pdo->query($sql);
     }catch(pdoException $e){
         echo "There was an error getting settings information from the database";
     }
-    $result = $statement->fetAll();
+    $result = $statement->fetchAll();
     
     foreach($result as $row){
         $siteName = $row['sitename'];
